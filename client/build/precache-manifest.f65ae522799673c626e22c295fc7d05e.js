5b41458cca12c7c5a8a4fa7451ac3ba6109c4992
omrichashper <114836028+omrichashper@users.noreply.github.com>
2022-12-04 21:47:41 -0500

    with build

---
+++ client/build/precache-manifest.f65ae522799673c626e22c295fc7d05e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84da499a15669f5901c5",
+    "url": "/cocktail-party-test/static/css/main.6523d075.chunk.css"
+  },
+  {
+    "revision": "84da499a15669f5901c5",
+    "url": "/cocktail-party-test/static/js/main.43908379.chunk.js"
+  },
+  {
+    "revision": "ab85835a4ad76d4090d4",
+    "url": "/cocktail-party-test/static/js/runtime~main.f01f7a13.js"
+  },
+  {
+    "revision": "a15113b0d9956991031f",
+    "url": "/cocktail-party-test/static/css/2.142112bb.chunk.css"
+  },
+  {
+    "revision": "a15113b0d9956991031f",
+    "url": "/cocktail-party-test/static/js/2.f638261d.chunk.js"
+  },
+  {
+    "revision": "eea3efe61797a8b86927",
+    "url": "/cocktail-party-test/static/js/3.15867bf6.chunk.js"
+  },
+  {
+    "revision": "c87fc08491d0e6025daf285212ba8fee",
+    "url": "/cocktail-party-test/index.html"
+  }
+];
